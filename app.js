4df7fc4818b9c5abcb666455e6c70c20277e2008
Nate Alcedo <ndaljr@gmail.com>
2016-12-19 20:33:01 +0800

    almost ready

--- app.js
+++ app.js
@@ -3,33 +3,35 @@
 const express = require('express');
 const pug = require('pug');
 const app = express();
-const mongo = require('mongoose');
+const mongo = require('mongodb');
+const api = require('./routes/api.js');
 
-app.set('view engine', 'pug');
-app.set('views', './');
+mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener', (err, db) => {
+	if (err) throw err;
+	console.log('connected to mongodb');
 
-app.get('/', (req, res) => {
-	res.render('index');
+	app.set('view engine', 'pug');
+	app.set('views', './');
+
+	db.createCollection('websites', {
+		capped: true,
+		size: 5242880,
+		max: 5000
+	});
+
+	app.get('/', (req, res) => {
+		res.render('index');
+	});
+
+	api(app,db);
+
+
+	let port = process.env.PORT || 8080;
+	app.listen(port, () => {
+
+		console.log('server started on port 8080');
+
+	});
+
 });
 
-app.get('/api/:url*', (req, res) => {
-
-	function isValidUrl(url) {
-        // Got this online
-		let regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-		return regex.test(url) ? true : false;
-	}
-
-	let url = req.url.replace('/api/', '');
-	if (isValidUrl(url)) {
-		res.send('valid');
-	} else {
-		res.json({
-            url: "Url is invalid. Enter a a properly formatted one"
-        });
-	}
-});
-
-app.listen(3000, () => {
-	console.log('server started on port 3000');
-});
