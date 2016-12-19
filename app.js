7cf1cbe0529a17203928e6007a9007ea2cf337d2
nathaniel d alcedo jr <ndaljr@gmail.com>
2016-12-19 17:53:36 +0800

    Init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const pug = require('pug')
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views','./')
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+app.get('/api/:url',(req,res)=>{
+    function isValidUrl(url){
+        let regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
+
+        regex.test(url) ? true: false
+    }
+
+    let url = req.params.url
+    // Validate url
+    // if valid make a new link in the database and link to it
+    if(isValidUrl(url)){
+        res.send('url is valid ')
+    }else{
+        let json = {
+            Error: "Url is not correct format"
+        }
+        res.json(json)
+    }
+})
+
+app.listen(3000)
